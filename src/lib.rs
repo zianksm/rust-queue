63a6ea0142302e5e0923137484edd51b3ad76111
zian <mynameiszian@gmail.com>
2023-04-25 20:47:17 +0800

    struct impl skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,27 @@ pub mod traits {
         fn new() -> Self;
     }
 }
+
+pub struct Queue<T> {
+    inner: Vec<T>,
+}
+
+impl<T> traits::QueueTrait<T> for Queue<T> {
+    fn dequeue(&self) -> Result<bool, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    fn enqueue(&self) -> Result<bool, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    fn inner(&self) -> Result<Vec<T>, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    fn new() -> Self {
+        let inner: Vec<T> = Vec::new();
+
+        Self { inner }
+    }
+}
